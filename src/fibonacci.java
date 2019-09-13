d82c018d661cfa16dcf881a8866c87e83cec68b7
LFalch <lucas@wasd.dk>
2019-09-14 00:02:34 +0200

    Reformat fibonacci

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,21 +1,14 @@
-
 public class fibonacci {
-	 
-		public static void main(String[] args) 
-		{
-			//da fibonacci serien hurtigt kommer over 31-bit, skal vi bruge #long og ikke #int
-			long n1=0,n2=1,n3,antal=49;
-				System.out.print("1");
-		        for (long i = 1; i <= antal; ++i)
-		        {
-		        	n3=n1+n2;
-		            System.out.print(" "+n3);
-		            n1=n2;
-		            n2=n3;
+	public static void main(String[] args) {
+		// Da Fibonacci-serien hurtigt kommer over 31-bit, skal vi bruge long i stedet for int
+		long n1 = 0, n2 = 1, antal = 49;
 
-
-		        }
-	 
+		System.out.print("1");
+		for (long i = 1; i <= antal; ++i) {
+			long n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
 		}
-	 
 	}
+}
